f9c322dd4e7fa7e921b36d73e6abab0d313cf1a6
Manishkumar-0912 <guessmeammanish@gmail.com>
2026-03-27 11:15:33 +0530

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-
+/*
 // HERO AUTO SLIDER
 
 const hero = document.querySelector(".hero");
@@ -20,3 +20,38 @@ function slider(){
 
 setInterval(slider, 4000);
 slider();
+*/
+
+const hero = document.querySelector(".hero");
+
+if (hero) {
+
+  const images = [
+    "images/poster1.jpg",
+    "images/poster2.jpg",
+    "images/poster3.jpg",
+    "images/Poster11.webp"
+  ];
+
+  // Preload images (no flicker)
+  images.forEach(src => {
+    const img = new Image();
+    img.src = src;
+  });
+
+  let i = 0;
+
+  function slider() {
+    hero.classList.add("fade");
+
+    setTimeout(() => {
+      hero.style.backgroundImage = `url(${images[i]})`;
+      i = (i + 1) % images.length;
+      hero.classList.remove("fade");
+    }, 400);
+  }
+
+  // Start slider
+  slider();
+  setInterval(slider, 4000);
+}
